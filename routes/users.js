f2438080ed080522ee4a287ea7a62b4648f103be
Яковлева Ксения <bezdenejnyhk@yandex.ru>
2023-05-13 18:10:18 +0300

    test4

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const usersRouter = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 const {
   getUsers,
@@ -10,8 +11,24 @@ const {
 
 usersRouter.get('/', getUsers);
 usersRouter.get('/me', getMe);
-usersRouter.get('/:userId', getUserById);
-usersRouter.patch('/me', editProfile);
-usersRouter.patch('/me/avatar', updateAvatar);
+
+usersRouter.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().hex().length(24).required(),
+  }),
+}), getUserById);
+
+usersRouter.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    about: Joi.string().min(2).max(30).required(),
+  }),
+}), editProfile);
+
+usersRouter.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required(),
+  }),
+}), updateAvatar);
 
 module.exports = usersRouter;
